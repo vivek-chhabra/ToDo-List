71808a270cc36beff2e8e83064a7ab4ec32152e5
vivek chhabra <vivekcb8642@gmail.com>
2023-05-28 17:16:31 +0530

    add todo component

--- ToDo List/src/ToDo.js
+++ ToDo List/src/ToDo.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import './ToDo.css'
 
 class ToDo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {pos: false}
+    }
+    handleClick = (evt) => {
+        if(this.state.pos === false) {
+            this.setState({pos: true})
+        } else {
+            this.setState({pos: false})
+        }
+    }
     render() {
         return(
-            <div className="ToDo">
-                
+            <div className="ToDo flex">
+                 <p onClick={this.handleClick} style={this.state.pos? {textDecoration: 'line-through'} : {textDecoration: 'none'} }> Milk the cat</p>
+                 <div className="ToDo-operations flex">
+                    <i className='fa-solid fa-edit' onClick={this.handleEdit}></i>
+                    <i className='fa-solid fa-trash' onClick={this.handleRemove}></i>
+                 </div>
             </div>
         )
     }
