9dc7f49fae813685fab15bd16774e261f28d49df
vivek chhabra <vivekcb8642@gmail.com>
2023-05-29 12:42:26 +0530

    add all components in the App component

--- ToDo List/src/App.js
+++ ToDo List/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import ToDoList from './ToDoList';
 import './App.css'
+import ToDo from './ToDo';
+import NewToDoForm from './NewToDoForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App flex">
       <ToDoList />
+      {/* <NewToDoForm /> */}
+      {/* <ToDo /> */}
     </div>
   );
 }
