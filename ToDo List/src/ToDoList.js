f135b295ea035cd17689622a0e813e9fd90dc077
vivek chhabra <vivekcb8642@gmail.com>
2023-05-29 12:19:26 +0530

    add ToDoList component

--- ToDo List/src/ToDoList.js
+++ ToDo List/src/ToDoList.js
@@ -1,12 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import "./ToDoList.css";
+import ToDo from "./ToDo";
+import NewToDoForm from "./NewToDoForm";
 
 class ToDoList extends Component {
-    render() {
-        return(
-            <div className="ToDoList">
+    constructor(props) {
+        super(props);
+        this.state = {
+            todoList: [],
+        };
+    }
 
+    handleSubmit = (newItem) => {
+        let newItm = { todo: newItem, key: crypto.randomUUID() };
+        this.setState((curState) => ({
+            todoList: [...curState.todoList, newItm],
+        }));
+    };
+
+    handleEdit = (id, newItem) => {
+        let updatedTodos = this.state.todoList.map((ele) => {
+            if (id === ele.key) {
+                return { todo: newItem, key: id };
+            }
+            return ele;
+        });
+        this.setState({todoList: updatedTodos})
+    };
+
+    handleRemove = (id) => {
+        this.setState((curState) => ({
+            todoList: curState.todoList.filter((ele) => ele.key != id),
+        }));
+    };
+
+    render() {
+        return (
+            <div className="ToDoList flex-column">
+                <div className="ToDoList-head flex-column">
+                    <h1>ToDo List!</h1>
+                    <p>A Simple ToDo List React App.</p>
+                </div>
+                <div className="list flex-column">
+                    {this.state.todoList.map((ele) => (
+                        <ToDo item={ele.todo} key={ele.key} id={ele.key} handleEdit={this.handleEdit} handleRemove={this.handleRemove} />
+                    ))}
+                </div>
+                <NewToDoForm handleSubmit={this.handleSubmit} key={crypto.randomUUID()} />
             </div>
-        )
+        );
     }
 }
 
