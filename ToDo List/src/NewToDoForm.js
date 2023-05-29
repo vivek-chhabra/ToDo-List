b353e8310e1e0400d995bd5fbf81ade95b1104c0
vivek chhabra <vivekcb8642@gmail.com>
2023-05-29 12:18:54 +0530

    add NewToDoForm component

--- ToDo List/src/NewToDoForm.js
+++ ToDo List/src/NewToDoForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react'
+import './NewToDoForm.css'
 
 class NewToDoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todo: ''
+        }
+    }
+    handleChange = (evt) => {
+        this.setState({todo: evt.target.value})
+    }
+    handleSubmit = (evt) => {
+        evt.preventDefault()
+        this.props.handleSubmit(this.state.todo)
+    }
     render() {
         return(
-            <div className="NewToDoForm">
-
+            <div className="NewToDoForm flex-column">
+                <label htmlFor="add-todo">New ToDo . . .</label>
+                <div className="NewToDoForm-add-todo flex">
+                    <form action="" className='flex' onSubmit={this.handleSubmit}> 
+                        <input type="text" placeholder='Add . . .' onChange={this.handleChange} required value={this.state.todo} name="" id="add-todo" />
+                        <button>ADD TODO</button>
+                    </form>
+                </div>
             </div>
         )
     }
